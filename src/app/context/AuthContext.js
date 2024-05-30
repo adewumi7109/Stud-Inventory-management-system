cb1c6621e76133de14e36cda47ca94d6c5408565
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-05-30 01:07:03 +0100

    fixes bug

--- src/app/context/AuthContext.js
+++ src/app/context/AuthContext.js
@@ -1,37 +1,52 @@
-"use client"
+"use client";
 import { createContext, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+
 const AuthContext = createContext({});
 
-export const AuthProvider = ({children})=>{
+export const AuthProvider = ({ children }) => {
     const [userData, setUserData] = useState(null);
     const router = useRouter();
+
+    useEffect(() => {
+        const storedUserData = localStorage.getItem('userData');
+        if (storedUserData) {
+            setUserData(JSON.parse(storedUserData));
+        }
+    }, []);
+
     const signin = (userData) => {
         const userDataString = JSON.stringify(userData);
-  setUserData(userData);
-  localStorage.setItem('userData', userDataString);
-      };
+        setUserData(userData);
+        if (typeof window !== "undefined") {
+            localStorage.setItem('userData', userDataString);
+        }
+    };
 
-      const getUserName =() =>{
-        const user = localStorage.getItem('userData')
-        const userData = user ? JSON.parse(user) : null;
-        if (userData && userData.response && userData.response.userName) {
-            return userData.response.userName;
-          } else {
-            return null; 
-          }
-    }
+    const getUserName = () => {
+        if (typeof window !== "undefined") {
+            const user = localStorage.getItem('userData');
+            const userData = user ? JSON.parse(user) : null;
+            if (userData && userData.response && userData.response.userName) {
+                return userData.response.userName;
+            }
+        }
+        return null;
+    };
 
-      const logout = () => {
+    const logout = () => {
         setUserData(null);
-        localStorage.removeItem('userData');
-        router.push('/')
-      };
-    return(
-        <AuthContext.Provider value={{signin, getUserName, logout, userData}}>
+        if (typeof window !== "undefined") {
+            localStorage.removeItem('userData');
+        }
+        router.push('/');
+    };
+
+    return (
+        <AuthContext.Provider value={{ signin, getUserName, logout, userData }}>
             {children}
         </AuthContext.Provider>
-    )
+    );
 }
 
 export default AuthContext;
